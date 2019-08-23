47f9f9cce44ecafe164a21d4f08bce1686e50e6a
Akshay Iyyadurai Balasundaram <ibakshay96@gmail.com>
2019-08-23 13:42:28 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core';
 async function run() {
   try {
     const myInput = core.getInput('myInput');
+    console.log('Testing if the logging works in GitHub Logging console')
     core.debug(`Hello ${myInput}`);
   } catch (error) {
     core.setFailed(error.message);
