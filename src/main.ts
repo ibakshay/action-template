64ea0e4b8a4d03a44804365002185e81a6882bfb
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-08-23 16:32:11 +0200

    Re: Environment setup for hydrated GitHub client

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github'
 
 async function run() {
   try {
     const myInput = core.getInput('myInput');
     core.debug(`Hello ${myInput}`);
+
+    // This should be a token with access to your repository scoped in as a secret.
+    const myToken = core.getInput('myToken');
+    const octokit = new github.GitHub(myToken);
+
+    const args = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.issue.number,
+      body: 'Hello World'
+    }
+    const responseToIssue = await octokit.issues.createComment(args)
+
+
   } catch (error) {
     core.setFailed(error.message);
   }
+
 }
 
 run();
