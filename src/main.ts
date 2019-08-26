52062fdf235071ffca82fa71d521fadf940e4a44
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-08-26 15:14:46 +0200

    Added  for all issue.created Event

--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,17 @@ async function run() {
     const myToken = core.getInput('myToken');
     const octokit = new github.GitHub(myToken);
 
-    if (github.context.payload.action === 'opened') {
-      const args = {
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: github.context.issue.number,
-        body: 'Thank you for creating the issue'
-      }
-
-      const responseToIssue = await octokit.issues.createComment(args)
-      console.error('Thank you for creating the issue')
+    //if (github.context.payload.action === 'opened') {
+    const args = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: github.context.issue.number,
+      body: 'Thank you for creating the issue'
     }
+
+    const responseToIssue = await octokit.issues.createComment(args)
+    console.error('Thank you for creating the issue')
+    //}
     console.error(github.context)
 
 
