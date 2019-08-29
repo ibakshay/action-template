78679a76f63346b31db4054cae6498e8e2f0df75
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-08-29 20:13:20 +0200

    Testing graphql query

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github'
-const graphQL = require('./graphql/query.ts')
+const graphQL = require('./graphql/query')
 
 async function run() {
   try {
