b3fc296e1d1c569dab6a8f2c425f873b286af853
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-01 09:13:26 +0200

    Testing If this app can read from a file

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,39 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github'
-//import { PRCommitters } from './graphQL/query'
+const fs = require('fs')
+const io = require('@actions/io')
 
 
 async function run() {
   try {
     const myInput = core.getInput('myInput');
-    console.log(`My input is  ${myInput}`);
+    console.log(`The path to CLA is   ${myInput}`);
 
     // This should be a token with access to your repository scoped in as a secret.
     const myToken = core.getInput('myToken');
     const octokit = new github.GitHub(myToken);
 
-    //if (github.context.payload.action === 'opened') {
     const args = {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       issue_number: github.context.issue.number
     }
-
-    // var graphql = new PRCommitters(args.owner, args.repo, args.issue_number, '')
-    // var query = graphql.getPRCommitters(args.owner, args.repo, args.issue_number, '')
-
-    // const { query, variables } = graphQL.getPRCommitters(args.owner, args.repo, args.issue_number, '')
     let variables = {
       owner: args.owner,
       name: args.repo,
       number: args.issue_number,
       cursor: ''
     }
-    //console.error(github.context)
+
+
+
+    /* Graphql start for getting committers */
     interface committersDetails {
       name: string,
       id: number
     }
-
     let committers: committersDetails[] = []
     const extractUserFromCommit = (commit) => commit.author.user || commit.committer.user || commit.author || commit.committer
-
     let response = await octokit.graphql(`
     query($owner:String! $name:String! $number:Int! $cursor:String!){
         repository(owner: $owner, name: $name) {
@@ -103,12 +99,15 @@ async function run() {
     });
 
     console.log(committers)
-
-    //const responseToIssue = await octokit.issues.createComment(args)
-    //  console.error('Thank you for creating the issue --dev-release')
-    //}
-
-
+    /* Graphql end for getting committers */
+    let pathToCla = '../cla.json'
+    let file
+    try {
+      file = fs.readFileSync(pathToCla, 'utf8');
+    } catch (err) {
+      throw new Error("CLA file not found.");
+    }
+    console.log("The contributors are :" + file)
 
   } catch (error) {
     core.setFailed(error.message);
