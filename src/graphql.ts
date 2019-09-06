5596a8cc8fc5cb7bf796ed62c9378cd08ba1b4fa
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-06 12:53:02 +0200

    Added return statement for graphql function

--- src/graphql.ts
+++ src/graphql.ts
@@ -60,7 +60,7 @@ export default async function getCommitters() {
                 name: committer.login || committer.name,
                 id: committer.databaseId || ''
             }
-            console.log(response)
+            console.log(committer)
             if (committers.length === 0 || committers.map((c) => {
                 return c.name
             }).indexOf(user.name) < 0) {
