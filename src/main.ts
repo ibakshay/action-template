33ac242f9772771f308bb48af4f31c6cedc0bd3b
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-11 14:32:24 +0200

    Testing Mongo persistence using github action

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import * as core from '@actions/core';
+import * as mongoose from 'mongoose'
+
+
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    console.log("CLA Assistant lite action is running")
+    mongoose.connect('mongodb://cla:AwS9DZkHb9rMBMqd@ds061076.mlab.com:27481/cla-assistant-lite-test', { useNewUrlParser: true })
+    const Schema = mongoose.Schema
+    const cla = new Schema({
+      username: { type: String, required: true, unique: true },
+      userid: { type: Number, required: false, unique: false }
+    })
+    const newModel = mongoose.model('clas', cla)
+    const newDocument = newModel({ username: 'ibakshay', userid: 13945 })
+    newDocument.save()
+
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
