1a716e2b36af4165c92771e200ca71783c5d3129
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-15 23:21:47 +0200

    testing graphql call

--- src/checkcla.ts
+++ src/checkcla.ts
@@ -14,7 +14,7 @@ export async function getclas() {
     }
     const branch = core.getInput('branch')
     let result, clas
-    const committers = await getCommitters()
+    const committers = getCommitters()
     console.log(committers)
     try {
         result = await octokit.repos.getContents({
