4ef4c843e3a81ff643d21a293d871242cf191874
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-17 17:22:15 +0200

    hopefully commemt will be created without any error with the committers name

--- src/checkcla.ts
+++ src/checkcla.ts
@@ -57,6 +57,7 @@ export async function getclas() {
     } catch (error) {
         if (error.status === 404) {
             committerMap.notSigned = committers
+            console.log(JSON.stringify(committerMap.notSigned))
             prComment(signed, committerMap)
             const initialContent = { signedContributors: [] }
             const initalContentString = JSON.stringify(initialContent, null, 2)
