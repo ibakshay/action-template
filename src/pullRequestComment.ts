5247fded6e327ec03bcbac721d421211ce146691
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-09-17 16:14:53 +0200

    comment on pr is done. Now lets test it

--- src/pullRequestComment.ts
+++ src/pullRequestComment.ts
@@ -30,29 +30,47 @@ async function getComment() {
 
 }
 
-function commentContent(signed: boolean, commiterMap?: CommitterMap) {
+function commentContent(signed: boolean, committerMap?: CommitterMap) {
     if (signed) {
         return `**CLA Assistant Lite** All committers have signed the CLA.`
     }
 
     let committersCount = 1
-    if (commiterMap && commiterMap.signed && commiterMap.notSigned) {
-        committersCount = commiterMap.signed.length + commiterMap.notSigned.length
+    if (committerMap && committerMap.signed && committerMap.notSigned) {
+        committersCount = committerMap.signed.length + committerMap.notSigned.length
     }
     let you = (committersCount > 1 ? 'you all' : 'you')
-    let text = `**CLA Assistant Lite:** <br/>Thank you for your submission, we really appreciate it. Like many open source projects, we ask that ${you} sign our [Contributor License Agreement](pathToCLADocument) before we can accept your contribution.<br/>`
-    console.log(text)
+    let text = `**CLA Assistant Lite:** <br/>Thank you for your submission, we really appreciate it. Like many open source projects, we ask that ${you} sign our [Contributor License Agreement](${pathToCLADocument}) before we can accept your contribution.<br/>`
+    if (committersCount > 1 && committerMap && committerMap.signed && committerMap.notSigned) {
+        text += `** ${committerMap.signed.length} ** out of ** ${committerMap.signed.length + committerMap.notSigned.length} ** committers have signed the CLA. <br/>`
+        committerMap.signed.forEach((signedCommitter) => {
+            text += `<br/>:white_check_mark: ${signedCommitter.name}`
+        })
+        committerMap.notSigned.forEach((unsignedCommitter) => {
+            text += `<br/>:x: ${unsignedCommitter.name}`
+        })
+        text += '<br/>'
+    }
+    if (committerMap && committerMap.unknown && committerMap.unknown.length > 0) {
+        let seem = (committerMap.unknown.length > 1 ? 'seem' : 'seems')
+        text += `<hr/>**${committerMap.unknown.join(', ')}** ${seem} not to be a GitHub user.`
+        text += ' You need a GitHub account to be able to sign the CLA. If you have already a GitHub account, please [add the email address used for this commit to your account](https://help.github.com/articles/why-are-my-commits-linked-to-the-wrong-user/#commits-are-not-linked-to-any-user).<br/>'
 
-    return `**CLA Assistant Lite:** Thank you for your submission, we really appreciate it. Like many open source projects, we ask that you sign our Contributor License Agreement before we can accept your contribution. You can respond with  :+1:  to this comment for signing the CLA`
+    }
+
+    if (committersCount)
+        console.log(text)
+
+    return text
 
 }
 
 
-export default async function prComment(signed: boolean, commiterMap?: CommitterMap) {
+export default async function prComment(signed: boolean, committerMap?: CommitterMap) {
     try {
-        console.log(JSON.stringify(commiterMap))
+        console.log(JSON.stringify(committerMap))
         const prComment = await getComment()
-        const body = commentContent(signed, commiterMap)
+        const body = commentContent(signed, committerMap)
         if (!signed && !prComment) {
             return octokit.issues.createComment({
                 owner: context.repo.owner,
