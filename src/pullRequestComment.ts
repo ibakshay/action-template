9a930c10d4c7dfaa7283d19cca63405c35ad9740
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-10-22 18:33:42 +0530

    test commit

--- src/pullRequestComment.ts
+++ src/pullRequestComment.ts
@@ -132,6 +132,7 @@ export default async function prComment(signed: boolean, committerMap: Committer
         // console.log("the reacted users are: " + JSON.stringify(reactedCommitters))
         // console.log(`The comment response is ${JSON.stringify(prComment)}  and the comment id is ${commentId}`)
         const body = commentContent(signed, committerMap)
+        console.log(signed + " and " + prComment)
         if (!signed && !prComment) {
             console.log("hello")
             // addLabel()
