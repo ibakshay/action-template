8c468761344890bb643885092378444032d149dd
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-11-12 10:23:45 +0100

    I got this

--- src/pullRequestComment.ts
+++ src/pullRequestComment.ts
@@ -121,6 +121,7 @@ async function reaction(commentId, committerMap: CommitterMap, committers) {
     })
     //checking if the reacted committers are not the signed committers(not in the storage file) and filtering out only the unsigned committers
     //TODO BUG: https://github.com/ibakshay/test-action-workflow/pull/120/checks?check_run_id=297679607
+    //TODO BUG: check if the reacted committers are the contributors of the same PR and then check if all the  CONTRIBUTORS have reacted
     reactedCommitters = committerMap.notSigned!.filter(committer => reactedCommitters.some(cla => committer.id === cla.id))
     console.log("the reacted users are: " + JSON.stringify(reactedCommitters))
     return reactedCommitters
@@ -153,12 +154,12 @@ export default async function prComment(signed: boolean, committerMap: Committer
             }
             const reactedCommitters = await reaction(prComment.id, committerMap, committers)
             //checking if all the unsigned committers have reacted to the PR comment (this is needed for changing the content of the PR comment to "All committers have signed the CLA")
-            //const reactedCommittersFlag = committers.some(committer => reactedCommitters.some(reactedCommitter => committer.id === reactedCommitter.id))
+            const reactedCommittersFlag = committers.some(committer => reactedCommitters.some(reactedCommitter => committer.id === reactedCommitter.id))
             //const reactedCommittersFlag = reactedCommitters.some(committer => committers.some(reactedCommitter => reactedCommitter.id === committer.id))
             //const result = !requiredfileds.some(i => !results.some(j => j.name === i.name));
             //const reactedCommittersFlag = !committers.some(committer => !reactedCommitters.some(reactedCommitter => reactedCommitter.id === committer.id))
             //const reactedCommittersFlag = committers.every(committer => reactedCommitters.some(reactedCommitter => committer.id === reactedCommitter.id))
-            const reactedCommittersFlag = reactedCommitters.every(reactedCommitter => committers.some(committer => reactedCommitter.id === committer.id))
+            //const reactedCommittersFlag = reactedCommitters.every(reactedCommitter => committers.some(committer => reactedCommitter.id === committer.id))
             // if (reactedCommittersFlag) {
             //     body = '**CLA Assistant Lite** All committers have signed the CLA. :smiley:'
             // }
