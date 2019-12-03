21a245553fe9488b2eca1b7c98bded37c8053206
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-12-03 15:56:20 +0100

    Development GitHub Actions

--- src/reaction.ts
+++ src/reaction.ts
@@ -31,12 +31,14 @@ export default async function reaction(commentId, committerMap: CommitterMap, co
                     ...reactedCommitter,
                     pullRequestNo: notSignedCommitter.pullRequestNo
                 }
+                bufferCommitters.push(reactedCommitter)
                 console.log('Becky  map is ' + JSON.stringify(reactedCommitter, null, 2))
+
             }
 
         })
     })
-    console.log('Akshay  map is ' + JSON.stringify(reactedCommitters, null, 2))
+    console.log('Akshay  map is ' + JSON.stringify(bufferCommitters, null, 2))
     //checking if the reacted users are only the contributors who has committed in the same PR (This is needed for the PR Comment and changing the status to success when all the contributors has reacted to the PR)
     reactedCommitterMap.onlyCommitters = committers.filter(committer => reactedCommitters.some(reactedCommitter => committer.id == reactedCommitter.id))
     console.log('reacted committers map is ' + JSON.stringify(reactedCommitterMap, null, 2))
