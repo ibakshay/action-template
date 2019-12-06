175439435de0ac7f186f365546b18de658f46e87
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-12-06 13:14:00 +0100

    Development GitHub Actions

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,26 @@ const io = require('@actions/io')
 import octokit from './octokit'
 import getCommitters from './graphql'
 import { getclas } from './checkcla'
+import { lockPullRequest } from './pullRequestLock'
 import { GitHub } from '@actions/github'
 
 
 
 async function run() {
   try {
+    //const lock = core.getInput('lock')
+    const pullRequestNo: number = context.issue.number
     core.debug('context payload is ' + JSON.stringify(context, null, 2))
     core.debug('CLA Assistant GitHub Action has started')
-    core.debug('the PR No is ' + JSON.stringify(context.issue.number))
-    //const myInput = core.getInput('myInput')
-    const clas = await getclas()
-    // const rateLimit = await octokit.rateLimit.get()
-    // console.log(JSON.stringify(rateLimit.data.resources, null, 4))
+    core.debug('the PR No is ' + JSON.stringify(pullRequestNo))
+    if (context.payload.action === 'closed') {
+      return lockPullRequest(pullRequestNo)
+    }
+    else {
+      await getclas()
+      // const rateLimit = await octokit.rateLimit.get()
+      // console.log(JSON.stringify(rateLimit.data.resources, null, 4))
+    }
 
   } catch (error) {
     core.setFailed(error.message)
