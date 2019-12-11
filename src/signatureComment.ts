b58f43a3af0f17dbf54acf74bcd68e490d13315f
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-12-11 14:43:29 +0100

    Development GitHub Actions

--- src/signatureComment.ts
+++ src/signatureComment.ts
@@ -17,7 +17,7 @@ async function webhookSmartContract(newSignedCommitters: CommittersDetails[]) {
         }
         const response = await fetch('https://9g6g7tied0.execute-api.eu-central-1.amazonaws.com/webhook', config)
         //const response = await res.json()
-        console.log("the response of the webhook is " + response)
+        console.log("the response of the webhook is " + JSON.stringify(response))
         if (response.success) {
             //return json
             return response
